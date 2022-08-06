7b3d8165dfd9a570b77c27bfd21e11491961e818
George-b1t <george.soares509@gmail.com>
2022-08-06 13:46:48 -0300

    Layout de rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/History'
 import { History } from './pages/Home'
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
